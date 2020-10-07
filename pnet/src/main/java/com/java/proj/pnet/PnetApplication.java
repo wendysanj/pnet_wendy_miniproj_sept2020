e9bd61c4b7a67f400272e7ab330400bb5ae1a67b
wendysanj <71631765+wendysanj@users.noreply.github.com>
2020-10-07 17:16:41 +0800

    Add files via upload

---
+++ pnet/src/main/java/com/java/proj/pnet/PnetApplication.java
@@ -0,0 +1,59 @@
+package com.java.proj.pnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.core.*;
+import org.springframework.http.*;
+import java.util.*;
+
+@SpringBootApplication
+public class PnetApplication {
+
+	@Autowired    
+	private CountryRepository countryRepository;    
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PnetApplication.class, args);
+	}
+
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+
+	// Consume external API
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {	 
+
+			HttpHeaders headers = new HttpHeaders();
+			headers.set("x-rapidapi-host", "restcountries-v1.p.rapidapi.com");
+			headers.set("x-rapidapi-key", "34cce90b94mshb79f9d2db4398d8p182f8djsnd9a6777a1371");
+			HttpEntity entity = new HttpEntity(headers);
+
+			HttpEntity<List <Country>> response = restTemplate.exchange(
+				"https://restcountries-v1.p.rapidapi.com/subregion/South-Eastern Asia", 
+				HttpMethod.GET, entity, new ParameterizedTypeReference<List <Country>> () {} );
+
+			List <Country> countryList = response.getBody();
+			//Save data.
+
+			 
+			countryRepository.saveAll(countryList);   ;
+			 
+			
+		};
+	}
+
+
+
+}
